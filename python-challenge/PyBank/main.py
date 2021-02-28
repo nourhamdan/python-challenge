230478788e7dca1bdaca902dcaf9f74060aefc1f
Nour hamdan <78324732+nourhamdan@users.noreply.github.com>
2021-02-27 19:24:55 -0600

    Add files via upload

--- python-challenge/PyBank/main.py
+++ python-challenge/PyBank/main.py
@@ -2,7 +2,7 @@ import csv
 import os
 
 #path to collect data
-budget_path = os.path.join("PyBank/Resources/budget_data.csv")
+budget_path = os.path.join(PyBank/Resources/budget_data.csv")
 budget_path_output = ("PyBank//Resources/budget_data.txt")
 
 # Variables to Track
@@ -25,7 +25,7 @@ with open(budget_path) as csvfile:
 
         
         total_months = total_months + 1
-        total_profit_loss = total_profit_loss + int(row["Profit/Losses"])
+        total_revenue = total_revenue + int(row["Profit/Losses"])
         print(row)
 
         # Keep track of changes
@@ -55,7 +55,7 @@ with open(budget_path) as csvfile:
     print("Financial Analysis")
     print("-------------------------")
     print("Total Months: " + str(total_months))
-    print("Total : " + "$" + str(total_profit_loss))
+    print("Total : " + "$" + str(total_revenue))
     print("Average Change: " + "$" + str(round(sum(profit_loss_changes) / len(profit_loss_changes),2)))
     print("Greatest Increase: " + str(greatest_increase[0]) + " ($" +  str(greatest_increase[1]) + ")") 
     print("Greatest Decrease: " + str(greatest_decrease[0]) + " ($" +  str(greatest_decrease[1]) + ")")
@@ -66,7 +66,7 @@ with open(budget_path) as csvfile:
 with open(budget_path_output, "w") as txt_file:
     txt_file.write("Total Months: " + str(total_months))
     txt_file.write("\n")
-    txt_file.write("Total: " + "$" + str(total_profit_loss))
+    txt_file.write("Total: " + "$" + str(total_revenue))
     txt_file.write("\n")
     txt_file.write("Average Change: " + "$" + str(round(sum(profit_loss_changes) / len(profit_loss_changes),2)))
     txt_file.write("\n")
